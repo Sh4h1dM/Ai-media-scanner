bdf5f99a98b07db8db92b28a3cca47321128db6c
Shahid.M <shahid.mulla1@outlook.com>
2026-06-15 14:14:04 +0100

    test

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from PIL import Image
+import os
+from PIL import Image # Security measure for corrupted files and validates media 
+import cv2 # Can process .mp4 and .avi file to read videos frame by frame
 
 filename = "istockphoto-2086363366-612x612.jpg"
 
