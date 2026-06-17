f718dc87fad5a8adad9891b9b503090dc5861400
Shahid.M <shahid.mulla1@outlook.com>
2026-06-17 18:15:26 +0100

    enhanced comments

--- main.py
+++ main.py
@@ -3,22 +3,27 @@ from PIL import Image # Security measure for corrupted files and validates media
 import cv2 # Can process .mp4 and .avi file to read videos frame by frame
 
 
-
+# Function 1 - handing file input and authentication before processing media 
 def scan_media(file_path):
-    
+
     """
     STAGE 1: Input Ingestion Gatekeeper
     Validates file existence, determines media type via extension extraction, 
     and routes the file to the appropriate processing pipeline (Pillow or OpenCV).
     """
 
+    # Variable check that scans the current directory for a file
     check = os.path.exists(file_path)
 
+    # If condition for error handling 
     if check is not True:
         print("File does not exist!")
         
+    # Else condition to extract the format of the file
     else:
         filename, extension = os.path.splitext(file_path)
+        # Extracts the extension to determine if media is static or moving text
+        
         print(f"File detected with extension : {extension}")
 
 
